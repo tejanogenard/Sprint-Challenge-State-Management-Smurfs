df0de649ff3c9109985dfde6ab88529beedab9c8
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-03-20 11:59:08 -0700

    update to actions

--- smurfs/src/actions/actionIndex.js
+++ smurfs/src/actions/actionIndex.js
@@ -1,5 +1,11 @@
+import axios from 'axios'
+
 export const GET_DATA = "GET_DATA"
 
 export const getData = () => dispatch =>{
 dispatch({type: GET_DATA})
+    axios.get(`http://localhost:3333/smurfs`)
+    .then(res => 
+        console.log(res))
+    .catch(err => console.log('error fetching data from api. err:', err))
 }
